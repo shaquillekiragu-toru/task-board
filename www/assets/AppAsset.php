a1edd853f7ccec3ce9a16d02821af229fb89bcc6
Shaquille Kiragu <shaquille@toru.digital>
2025-05-29 14:10:56 +0100

    Create TailwindAsset class

--- www/assets/AppAsset.php
+++ www/assets/AppAsset.php
@@ -2,5 +2,11 @@
 
 namespace www\assets;
 
-class AppAsset extends \common\assets\AppAsset {
+use yii\web\AssetBundle;
+
+class TailwindAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = ['/css/tailwind.css?v=' . CACHE_KEY];
 }
