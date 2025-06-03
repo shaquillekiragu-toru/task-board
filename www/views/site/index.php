c202fb860046e16a4568dc2b0de2667e5ab2dd2c
Shaquille Kiragu <shaquille@toru.digital>
2025-06-03 11:28:31 +0100

    Build UI

--- www/views/site/index.php
+++ www/views/site/index.php
@@ -1,13 +1,150 @@
 <div class="container mx-auto">
-    <h1 class="mb-5">Task Board</h1>
-    <div class="grid grid-cols-3 gap-4 p-4 border-2 border-black rounded-md">
-        <div class="flex flex-col gap-2 border-2 border-black rounded-md">
-            <? foreach ($tasks as $task) { ?>
-                <div class="flex flex-col gap-2 p-2 border border-black rounded-md">
-                    <h2 class="text-lg font-bold"><?= $task->title ?></h2>
-                    <p class="text-sm text-gray-500"><?= $task->description ?></p>
+    <div class="w-full flex justify-end mb-5">
+        <button class="bg-green-500 text-white px-4 py-2 rounded-md">Create a New Task</button>
+    </div>
+    <h1 class="w-full text-center mb-10 text-4xl font-bold">Task Board</h1>
+    <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-12 border-2 border-black rounded-xl">
+        <div class="flex flex-col gap-2 p-8 border-2 border-black rounded-lg min-h-[500px]">
+            <h2 class="w-full text-center text-xl font-bold mb-5">To Do</h2>
+            <div class="w-full flex flex-col gap-2 p-4 border border-black rounded-lg bg-white">
+                <h3 class="text-lg font-bold">Title 1</h3>
+                <div class="flex justify-between">
+                    <p class="text-sm text-gray-500">Description 1</p>
+                    <p class="text-sm text-gray-500">Due: 2025-06-03</p>
                 </div>
-            <? } ?>
+                <div class="flex justify-between">
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Edit</button>
+                    <button class="bg-red-500 text-white px-4 py-2 rounded-md">Delete</button>
+                </div>
+            </div>
+            <div class="w-full flex flex-col gap-2 p-4 border border-black rounded-lg bg-white">
+                <h3 class="text-lg font-bold">Title 2</h3>
+                <div class="flex justify-between">
+                    <p class="text-sm text-gray-500">Description 2</p>
+                    <p class="text-sm text-gray-500">Due: 2025-06-03</p>
+                </div>
+                <div class="flex justify-between">
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Edit</button>
+                    <button class="bg-red-500 text-white px-4 py-2 rounded-md">Delete</button>
+                </div>
+            </div>
+            <div class="w-full flex flex-col gap-2 p-4 border border-black rounded-lg bg-white">
+                <h3 class="text-lg font-bold">Title 3</h3>
+                <div class="flex justify-between">
+                    <p class="text-sm text-gray-500">Description 3</p>
+                    <p class="text-sm text-gray-500">Due: 2025-06-03</p>
+                </div>
+                <div class="flex justify-between">
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Edit</button>
+                    <button class="bg-red-500 text-white px-4 py-2 rounded-md">Delete</button>
+                </div>
+            </div>
+            <div class="w-full flex flex-col gap-2 p-4 border border-black rounded-lg bg-white">
+                <h3 class="text-lg font-bold">Title 4</h3>
+                <div class="flex justify-between">
+                    <p class="text-sm text-gray-500">Description 4</p>
+                    <p class="text-sm text-gray-500">Due: 2025-06-03</p>
+                </div>
+                <div class="flex justify-between">
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Edit</button>
+                    <button class="bg-red-500 text-white px-4 py-2 rounded-md">Delete</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="flex flex-col gap-2 p-8 border-2 border-black rounded-lg min-h-[500px]">
+            <h2 class="w-full text-center text-xl font-bold mb-5">In Progress</h2>
+            <div class="w-full flex flex-col gap-2 p-4 border border-black rounded-lg bg-white">
+                <h3 class="text-lg font-bold">Title 5</h3>
+                <div class="flex justify-between">
+                    <p class="text-sm text-gray-500">Description 5</p>
+                    <p class="text-sm text-gray-500">Due: 2025-06-03</p>
+                </div>
+                <div class="flex justify-between">
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Edit</button>
+                    <button class="bg-red-500 text-white px-4 py-2 rounded-md">Delete</button>
+                </div>
+            </div>
+            <div class="w-full flex flex-col gap-2 p-4 border border-black rounded-lg bg-white">
+                <h3 class="text-lg font-bold">Title 6</h3>
+                <div class="flex justify-between">
+                    <p class="text-sm text-gray-500">Description 6</p>
+                    <p class="text-sm text-gray-500">Due: 2025-06-03</p>
+                </div>
+                <div class="flex justify-between">
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Edit</button>
+                    <button class="bg-red-500 text-white px-4 py-2 rounded-md">Delete</button>
+                </div>
+            </div>
+            <div class="w-full flex flex-col gap-2 p-4 border border-black rounded-lg bg-white">
+                <h3 class="text-lg font-bold">Title 7</h3>
+                <div class="flex justify-between">
+                    <p class="text-sm text-gray-500">Description 7</p>
+                    <p class="text-sm text-gray-500">Due: 2025-06-03</p>
+                </div>
+                <div class="flex justify-between">
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Edit</button>
+                    <button class="bg-red-500 text-white px-4 py-2 rounded-md">Delete</button>
+                </div>
+            </div>
+            <div class="w-full flex flex-col gap-2 p-4 border border-black rounded-lg bg-white">
+                <h3 class="text-lg font-bold">Title 8</h3>
+                <div class="flex justify-between">
+                    <p class="text-sm text-gray-500">Description 8</p>
+                    <p class="text-sm text-gray-500">Due: 2025-06-03</p>
+                </div>
+                <div class="flex justify-between">
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Edit</button>
+                    <button class="bg-red-500 text-white px-4 py-2 rounded-md">Delete</button>
+                </div>
+            </div>
+        </div>
+
+        <div class="flex flex-col gap-2 p-8 border-2 border-black rounded-lg min-h-[500px]">
+            <h2 class="w-full text-center text-xl font-bold mb-5">Done</h2>
+            <div class="w-full flex flex-col gap-2 p-4 border border-black rounded-lg bg-white">
+                <h3 class="text-lg font-bold">Title 9</h3>
+                <div class="flex justify-between">
+                    <p class="text-sm text-gray-500">Description 9</p>
+                    <p class="text-sm text-gray-500">Due: 2025-06-03</p>
+                </div>
+                <div class="flex justify-between">
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Edit</button>
+                    <button class="bg-red-500 text-white px-4 py-2 rounded-md">Delete</button>
+                </div>
+            </div>
+            <div class="w-full flex flex-col gap-2 p-4 border border-black rounded-lg bg-white">
+                <h3 class="text-lg font-bold">Title 10</h3>
+                <div class="flex justify-between">
+                    <p class="text-sm text-gray-500">Description 10</p>
+                    <p class="text-sm text-gray-500">Due: 2025-06-03</p>
+                </div>
+                <div class="flex justify-between">
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Edit</button>
+                    <button class="bg-red-500 text-white px-4 py-2 rounded-md">Delete</button>
+                </div>
+            </div>
+            <div class="w-full flex flex-col gap-2 p-4 border border-black rounded-lg bg-white">
+                <h3 class="text-lg font-bold">Title 11</h3>
+                <div class="flex justify-between">
+                    <p class="text-sm text-gray-500">Description 11</p>
+                    <p class="text-sm text-gray-500">Due: 2025-06-03</p>
+                </div>
+                <div class="flex justify-between">
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Edit</button>
+                    <button class="bg-red-500 text-white px-4 py-2 rounded-md">Delete</button>
+                </div>
+            </div>
+            <div class="w-full flex flex-col gap-2 p-4 border border-black rounded-lg bg-white">
+                <h3 class="text-lg font-bold">Title 12</h3>
+                <div class="flex justify-between">
+                    <p class="text-sm text-gray-500">Description 12</p>
+                    <p class="text-sm text-gray-500">Due: 2025-06-03</p>
+                </div>
+                <div class="flex justify-between">
+                    <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Edit</button>
+                    <button class="bg-red-500 text-white px-4 py-2 rounded-md">Delete</button>
+                </div>
+            </div>
         </div>
     </div>
-</div>
