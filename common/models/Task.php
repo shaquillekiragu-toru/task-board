1e8a16f30471f0af4cb6d230ae428851bdf27f96
Shaquille Kiragu <shaquille@toru.digital>
2025-06-03 11:26:20 +0100

    Update task model - fetch data

---
+++ common/models/Task.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace common\models;
+
+class Task extends \common\models\RestModel
+{
+    public static function tableName()
+    {
+        return '{{%task}}';
+    }
+
+    public function rules()
+    {
+        return array_merge(parent::rules(), [
+            [['title', 'description', 'status'], 'string'],
+            [['due_date', 'created_at'], 'integer'],
+            [['assigned_user_id'], 'integer'],
+        ]);
+    }
+
+    public function getTitle(): string
+    {
+        return $this->title ?? '';
+    }
+
+    public function getSubtitle(): string
+    {
+        return $this->description ?? '';
+    }
+}
