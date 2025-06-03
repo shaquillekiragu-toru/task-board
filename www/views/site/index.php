7ff3bc6d84ff0e09c2248c030ee6e2e324835251
Shaquille Kiragu <shaquille@toru.digital>
2025-06-03 12:36:47 +0100

    Build UI 4

--- www/views/site/index.php
+++ www/views/site/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use yii\helpers\Html;
+use yii\helpers\Url;
 
 ?>
 
@@ -8,11 +9,13 @@ use yii\helpers\Html;
     <div class="w-full flex justify-end mb-5">
         <button class="bg-green-500 text-white px-4 py-2 rounded-md">Create a New Task</button>
     </div>
-    <h1 class="w-full text-center mb-10 text-4xl font-bold">Task Board</h1>
-    <div class="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 p-12 border-2 border-black rounded-xl">
+    <div class="w-full flex justify-center mb-10">
+        <h1 class="text-4xl font-bold">Task Board</h1>
+    </div>
+    <div class="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 p-12 border-2 border-black rounded-xl">
 
         <div class="flex flex-col gap-4 p-8 border-2 border-black rounded-lg min-h-[500px]">
-            <h2 class="w-full text-center text-xl font-bold mb-5">To Do</h2>
+            <h2 class="w-full text-center text-2xl font-bold mb-5">To Do</h2>
             <div class="w-full flex flex-col gap-4 p-4 border border-black rounded-lg bg-white">
                 <div class="w-full flex justify-between">
                     <p class="text-sm text-blue-500">User 1</p>
@@ -80,7 +83,7 @@ use yii\helpers\Html;
         </div>
 
         <div class="flex flex-col gap-4 p-8 border-2 border-black rounded-lg min-h-[500px]">
-            <h2 class="w-full text-center text-xl font-bold mb-5">In Progress</h2>
+            <h2 class="w-full text-center text-2xl font-bold mb-5">In Progress</h2>
             <div class="w-full flex flex-col gap-4 p-4 border border-black rounded-lg bg-white">
                 <div class="w-full flex justify-between">
                     <p class="text-sm text-blue-500">User 5</p>
@@ -148,7 +151,7 @@ use yii\helpers\Html;
         </div>
 
         <div class="flex flex-col gap-4 p-8 border-2 border-black rounded-lg min-h-[500px]">
-            <h2 class="w-full text-center text-xl font-bold mb-5">Done</h2>
+            <h2 class="w-full text-center text-2xl font-bold mb-5">Done</h2>
             <div class="w-full flex flex-col gap-4 p-4 border border-black rounded-lg bg-white">
                 <div class="w-full flex justify-between">
                     <p class="text-sm text-blue-500">User 9</p>
@@ -214,6 +217,5 @@ use yii\helpers\Html;
                 </div>
             </div>
         </div>
-
     </div>
 </div>
