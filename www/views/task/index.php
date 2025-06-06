87aadd765961cb2d550430f6646dd4dc6b7e2ab9
Shaquille Kiragu <shaquille@toru.digital>
2025-06-06 12:00:31 +0100

    Format dates and update responsiveness styling

--- www/views/task/index.php
+++ www/views/task/index.php
@@ -5,22 +5,22 @@ use yii\helpers\Url;
 
 ?>
 
-<div class="container mx-auto">
+<div class="max-w-none md:container mx-auto px-10">
     <div class="w-full flex justify-end mb-5">
         <a href="<?= Url::to(['task/create']) ?>" class="bg-green-500 text-white px-4 py-2 rounded-md">Create a New Task</a>
     </div>
     <div class="w-full flex justify-center mb-10">
         <h1 class="text-4xl font-bold">Task Board</h1>
     </div>
-    <div class="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 p-12 border-2 border-black rounded-xl">
+    <div class="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 p-8 md:p-12 border-2 border-black rounded-xl">
         <?php foreach ($tasksByStatus as $status => $tasks) { ?>
-            <div class="flex flex-col gap-4 p-8 border-2 border-black rounded-lg min-h-[500px]">
+            <div class="flex flex-col gap-4 p-6 md:p-8 border-2 border-black rounded-lg min-h-[500px]">
                 <h2 class="w-full text-center text-2xl font-bold mb-5"><?= $status ?></h2>
                 <?php foreach ($tasks as $task) : ?>
                     <div class="w-full flex flex-col gap-6 p-4 border border-black rounded-lg bg-white">
                         <div class="w-full flex justify-around">
-                            <p class="text-sm text-gray-500">Created: <?= $task->created_at ?></p>
-                            <p class="text-sm text-gray-500">Due: <?= $task->due_date ?></p>
+                            <p class="text-sm text-gray-500">Created: <?= $task->formattedCreatedAt ?></p>
+                            <p class="text-sm text-gray-500">Due: <?= $task->formattedDueDate ?></p>
                         </div>
                         <div class="w-full flex justify-center">
                             <p class="text-lg font-bold"><?= $task->title ?></p>
