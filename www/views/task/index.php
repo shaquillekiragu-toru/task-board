303d067d7410be327f81dad4efa1d20c7ec04148
Shaquille Kiragu <shaquille@toru.digital>
2025-06-09 18:49:11 +0100

    Change foreach loop syntax

--- www/views/task/index.php
+++ www/views/task/index.php
@@ -16,7 +16,7 @@ use yii\helpers\Url;
         <?php foreach ($tasksByStatus as $status => $tasks) { ?>
             <div class="flex flex-col gap-4 p-6 md:p-8 border-2 border-black rounded-lg min-h-[500px]">
                 <h2 class="w-full text-center text-2xl font-bold mb-5"><?= $status ?></h2>
-                <?php foreach ($tasks as $task) : ?>
+                <?php foreach ($tasks as $task) { ?>
                     <div class="w-full flex flex-col gap-6 p-4 border border-black rounded-lg bg-white">
                         <div class="w-full flex justify-around">
                             <p class="text-sm text-gray-500">Created: <?= $task->formattedCreatedAt ?></p>
@@ -36,7 +36,7 @@ use yii\helpers\Url;
                             <a href="<?= Url::to(['task/delete', 'id' => $task->id]) ?>" class="bg-red-500 text-white px-4 py-2 rounded-md">Delete</a>
                         </div>
                     </div>
-                <?php endforeach; ?>
+                <?php } ?>
             </div>
         <?php } ?>
     </div>
